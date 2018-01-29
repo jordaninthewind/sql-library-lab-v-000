d9729f150bb1a4884c45adc192e30f7563389dd0
Jordan Kline <jordan.kline@gmail.com>
2018-01-29 21:36:43 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,7 +21,7 @@ def select_series_title_with_most_human_characters
   "SELECT Series.title FROM series
    INNER JOIN characters ON Characters.author_id = Series.author_id
    WHERE Characters.species = 'human'
-   GROUP BY species ORDER BY COUNT(Characters.species) DESC;"
+   ORDER BY COUNT(Characters.species) DESC;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
